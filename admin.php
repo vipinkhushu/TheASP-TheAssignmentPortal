dcaa0eba7038c5b43e91e1d7ae9f4345dfc190f3
Vipin Khushu <vipinkhushu@hotmail.com>
2015-08-16 15:40:38 +0530

    logout,profile,password link added in admin page which was earlier missing

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT `type` FROM `user_information` where username='$user_check'";
 $result = $conn->query($sql);
@@ -22,10 +22,10 @@ if ($result->num_rows > 0) {
 
 header("location: home.php");
 
-	 
-		
-	   } 
-	    
+
+
+	   }
+
 	   }
 }
 $conn->close();
@@ -78,71 +78,71 @@ $conn->close();
             <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;&nbsp;Home</a></li>
             <li><a href="aboutasp.php"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>&nbsp;&nbsp;About</a></li>
             <li><a href="contact.php"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>&nbsp;&nbsp;Contact</a></li>
-			
+
 			<!------------LOGIN SYSTEM-------------------->
-		
-
-		
 
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<!------------LOGIN SYSTEM-------------------->
-	
+
 	<li><a href="" data-toggle="dropdown"><span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>&nbsp;&nbsp;More</a>
-	<ul class="dropdown-menu" role="menu" aria-labelledby="dlabel">
-			<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;News Letter</li>
-			<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;FAQ</li>
-			<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;T&C</li>
-			
-			</ul>
-	
+    <ul class="dropdown-menu" role="menu" aria-labelledby="dlabel">
+  		<a href="contact.php?newsletter=subscribe">	<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;News Letter</li></a>
+  			<a href="faq.php">	<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;FAQ</li></a>
+  			<a href="termsofuse.php">	<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;T&C</li></a>
+
+  			</ul>
+
 	</li>
 
 	<li class="pull-right"><a href="" data-toggle="dropdown">
-	
+
 	<script type="text/javascript">
-    
-	var vip='<?php     
-	
+
+	var vip='<?php
+
 	echo md5($user_check);
 	?>';
-    
 
-		
+
+
         data = new Identicon(vip, 420).toString();
         document.write(
                 '<img alt="TheASP Avtaar"  width=21 height=21 src="data:image/png;base64,' + data + '">'
         );
-    
+
 </script>
 	&nbsp;&nbsp;<?php echo $user_check; ?> <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span></a>
-	<ul class="dropdown-menu" role="menu" aria-labelledby="dlabel">
-		<a href="contact.php?newsletter=subscribe">	<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;News Letter</li></a>
-			<a href="faq.php">	<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;FAQ</li></a>
-			<a href="termsofuse.php">	<li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>&nbsp;&nbsp;T&C</li></a>
-			
-			</ul>
-	
+
+      <ul class="dropdown-menu" role="menu" aria-labelledby="dlabel">
+    			<a href="profile.php"><li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;My Profile</li></a>
+    			<a href="settings.php"><li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>&nbsp;&nbsp;Change Password</li></a>
+    			<a href="logout.php"><li style="background:#fff;padding:6px 6px 6px 6px;border-bottom:0px solid black;"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>&nbsp;&nbsp;Logout</li></a>
+
+    			</ul>
 	</li>
 
-	
+
           </ul>
-		 
+
 
         </div><!--/.nav-collapse -->
-       
+
 	  </div>
-	  
+
     </nav>
 
 
@@ -166,14 +166,14 @@ $conn->close();
       <div class="modal-body">
      <div class="alert alert-warning alert-dismissible fade in" role="alert">
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-	<span aria-hidden="true">×</span></button>It is strongly recommended To Set<strong> Security Question</strong> As Soon As Possible</div>
-		
+	<span aria-hidden="true">?</span></button>It is strongly recommended To Set<strong> Security Question</strong> As Soon As Possible</div>
+
 		   <form method="post" action="securityquestion.php">
     <div class="form-group">
     <label for="username">Security Question</label>
 	<div class="input-group">
 	<div class="input-group-addon"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></div>
-   
+
  <select name="question" class="form-control" required>
  <option value="1">What is the name of your favourite actor?   </option>
   <option value="2">What is the name of your favourite book?   </option>
@@ -181,9 +181,9 @@ $conn->close();
    <option value="4">Which brand do you prefer to wear?   </option>
     <option value="5">What admires you the most?   </option>
  </select>
-	
+
 	</div>
-	
+
   </div>
   <div class="form-group">
     <label for="password">Security Answer</label>
@@ -191,15 +191,15 @@ $conn->close();
 	<div class="input-group-addon"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></div>
     <input type="password" class="form-control" id="answer" name="answer" placeholder="Enter Your Answer Here..." required>
   </div></div>
-     
+
 
       </div>
       <div class="modal-footer">
-        
+
         <input class="btn btn-primary" type="submit" id="" value="Submit">
       </div>
 	  </form>
-	  
+
     </div>
   </div>
 </div>
@@ -207,7 +207,7 @@ $conn->close();
 			  <center><span style="color:red;background:#fff;"><noscript>
 			  <span class="glyphicon glyphicon-alert" aria-hidden="true"></span><br>
 			  JavaScript is disabled in your browser, Kindly Enable it to have a better experience using TheASP
-			 
+
 			  Or Update your browser
 			  </noscript></span></center>
 		<div class="container">
@@ -215,43 +215,43 @@ $conn->close();
   <div class="col-md-3">
   <div class="regpage">
   <center>
-  
+
   <!--------------IDENTICONAPI----------->
-      
+
 <script type="text/javascript">
-    
-	var vip='<?php     
-	
+
+	var vip='<?php
+
 	echo md5($user_check);
 	?>';
-    
 
-		
+
+
         data = new Identicon(vip, 420).toString();
         document.write(
                 '<br><img alt="TheASP Avtaar" class="img-thumbnail" width=400 height=400 src="data:image/png;base64,' + data + '">'
         );
-    
+
 </script>
-		
-       
 
-	
- 
 
-		
-  
 
-  
-  
 
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
   </center>
-  
+
   <?php
 include('pass.php');
 
@@ -260,7 +260,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT name,college,rollno FROM `user_information` where username='$user_check'";
 $result = $conn->query($sql);
@@ -276,8 +276,8 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  
-  
+
+
   <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
   <div class="panel panel-default">
     <div class="panel-heading" role="tab" id="headingOne">
@@ -330,7 +330,7 @@ $conn->close();
 </ul>
      </div>
     </div>
-	
+
   </div>
   <div class="panel panel-default">
     <div class="panel-heading" role="tab" id="headingFour">
@@ -353,21 +353,21 @@ $conn->close();
     </div>
   </div>
 </div>
-  
-  
+
+
   <hr>
   <center>
-|| TheASP Premium Version || <br>|| © RAPL IND. - 2015 || <br>|| <a href="aboutasp.php">ABOUT US ||</a>
+|| TheASP Premium Version || <br>|| ? RAPL IND. - 2015 || <br>|| <a href="aboutasp.php">ABOUT US ||</a>
 <br><a href="https://www.facebook.com/theasp.rapl" target="_new"><img src="images/facebook.png" width="10%" height="10%"></a>
 </center>
-  
-  
-  
-  
-  
+
+
+
+
+
   </div>
   </div>
-  
+
   <div class="col-md-9">
   <div class="regpage">
 
@@ -382,9 +382,9 @@ $conn->close();
   <!-- Tab panes -->
   <div class="tab-content">
     <div role="tabpanel" class="tab-pane fade in active" id="home">
-	
+
 	<table class="table table-bordered">
-	
+
 	<?php
 include('pass.php');
 
@@ -393,7 +393,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT * FROM `user_information`";
 $result = $conn->query($sql);
@@ -406,36 +406,36 @@ $num=1;
 	   if($row["type"]=="admin")
 		   echo"<span class='glyphicon glyphicon-fire' aria-hidden='true'></span>";
 	   else if($row["type"]=="stud")
-		   echo"<span class='glyphicon glyphicon-user' aria-hidden='true'></span>";	
+		   echo"<span class='glyphicon glyphicon-user' aria-hidden='true'></span>";
 	   else if($row["type"]=="college")
 		   echo"<span class='glyphicon glyphicon-education' aria-hidden='true'></span>";
 	   else if($row["type"]=="teacher")
-		   echo"<span class='glyphicon glyphicon-blackboard' aria-hidden='true'></span>";	   
+		   echo"<span class='glyphicon glyphicon-blackboard' aria-hidden='true'></span>";
 	   echo"</td><td>".$row["username"]."</td><td>".$row["name"]."</td><td>".$row["email"]."</td>
 	   <td><form action='admin.php' method='get'><input type='text' name='id' value=".$row["id"]." hidden>
 	   <input type='submit' value='Get Details' class='btn btn-primary'></form></td></tr>";
 $num+=1;
     }
-} else 
+} else
 {
   echo"No Accounts yet";
 }
 $conn->close();
 ?>
-	</table> 
-	
+	</table>
 
 
 
- 
+
+
 	</div>
     <div role="tabpanel" class="tab-pane fade" id="class">
 
      <!----------------------->
 
-    	
+
 	<table class="table table-bordered">
-	
+
 	<?php
 include('pass.php');
 
@@ -444,7 +444,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT * FROM `logs` WHERE `type`='contact'";
 $result = $conn->query($sql);
@@ -454,7 +454,7 @@ if ($result->num_rows > 0) {
 		<td><strong>Status</strong></td></tr>";
 $num=1;
     while($row = $result->fetch_assoc()) {
-       echo"<tr><td>$num</td><td>".$row["id"]."</td>";   
+       echo"<tr><td>$num</td><td>".$row["id"]."</td>";
 	   echo"<td>".$row["username"]."<br/>".$row["ip"]."</td><td>".$row["time_stamp"]."</td><td>".$row["feedback_type"]."</td>
 	   <td>".$row["fullname"]."<br/>".$row["email"]."<br/>".$row["mobile"]."</td>
 	   <td>".$row["message"]."</td>
@@ -470,30 +470,30 @@ $num=1;
 	   {
 		   echo"Solved";
 	   }
-	   
+
 	   echo"</td>
 	   </tr>";
 $num+=1;
     }
-} else 
+} else
 {
   echo"No Accounts yet";
 }
 $conn->close();
 ?>
-	</table> 
+	</table>
 	<!----------------------->
 	  </div>
-	
-	
+
+
 	</div>
-    
+
 
   </div>
 
 </div>
 
-  
+
   </div>
   </div>
 </div>
@@ -510,9 +510,9 @@ if(isset($_GET["id"]))
 }
 
 ?>
- 
- 
- 
+
+
+
  <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -524,7 +524,7 @@ if(isset($_GET["id"]))
       </div>
       <div class="modal-body">
 	<div class='alert alert-info alert-dismissible fade in' role='alert'>
-	
+
 	<?php
 	if(isset($_GET["id"]))
 	{
@@ -535,20 +535,20 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT * FROM `user_information` WHERE `id`=$getid";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-   echo"<p>Phone Number : ".$row["phoneno"]."</p>"; 
+   echo"<p>Phone Number : ".$row["phoneno"]."</p>";
 echo"<p>College Name : ".$row["college"]."</p>";
-echo"<p>Class : ".$row["course"].",".$row["batch"].",".$row["rollno"]."</p>";    
+echo"<p>Class : ".$row["course"].",".$row["batch"].",".$row["rollno"]."</p>";
  echo"<p>Security: ".$row["squestion"]."(".$row["sanswer"].")</p>";
-echo"<p>Activation Status : ".$row["activate"]."</p>"; 
+echo"<p>Activation Status : ".$row["activate"]."</p>";
 echo"<p>Registration Time : ".$row["reg_time"]."</p>";
-}} else 
+}} else
 {
   echo"No Info Available";
 }
@@ -557,8 +557,8 @@ $conn->close();
 
 
 ?>
-	
-	
+
+
 	</div>
       </div>
       <div class="modal-footer">
