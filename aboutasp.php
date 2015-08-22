d6b1866097c59c19c0aa91e806f2ceb88f28272f
Vipin Khushu <vipinkhushu@hotmail.com>
2015-08-22 14:24:28 +0530

    fb comment box added for testimonials

--- aboutasp.php
+++ aboutasp.php
@@ -395,14 +395,20 @@ Welcome to the site, it seems connection is all it takes. One step closer to get
 	
   <hr>
  <h3><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>&nbsp;Testimonials</h3>
- <blockquote>
-	<p>"<em>Message Here</em>"</p>
-	<footer>By NAME(DETAILS)</footer>
-	</blockquote>
- <blockquote>
-	<p>"<em>Message Here</em>"</p>
-	<footer>By NAME(DETAILS)</footer>
-	</blockquote>
+ <!-----------------FB COMMENT BOX--------------->
+ <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=673459569433037";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-comments" data-href="http://theasp.tk" data-width="100%" data-numposts="10"></div>
+ 
+ 
+ 
+ <!------------------------------------------------>
  </div>
   
 </div>
