011d6d7ecc5da2e1f4984351164934a1f0e85b00
Vipin Khushu <vipinkhushu@hotmail.com>
2015-08-25 12:54:19 +0530

    Code to deactivate teachers account

---
+++ deactteacher.php
@@ -0,0 +1,34 @@
+<?php
+include('pass.php');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+if(isset($_POST['actname']))
+{
+$id=$_POST['actname'];	
+
+$sql="UPDATE `user_information` SET `activate` = '0' WHERE `id` = '$id';"; 
+		if ($conn->query($sql) === TRUE) {
+		header("location: college.php?msg=deactSuccess");
+		//echo "Error:<br>Error Details:-  " . $sql . "<br>" . $conn->error;
+		}
+else 
+{
+   header("location: college.php?msg=ErrorAct");
+}
+
+}
+
+
+else 
+{
+   header("location: college.php?msg=ErrorAct");
+}
+
+
+$conn->close();
+?>
